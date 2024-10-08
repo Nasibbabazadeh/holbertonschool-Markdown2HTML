fdcdab7daf875b55a0074303b80d269ce1a415d4
Nasib Babazade <143509503+Nasib-cmd@users.noreply.github.com>
2024-10-08 19:43:26 +0400

    task1 setup

---
+++ markdown2html.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+import os
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
+    
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+    
+    if not os.path.exists(markdown_file):
+        sys.stderr.write(f"Missing {markdown_file}\n")
+        sys.exit(1)
+    
+    
+    sys.exit(0)
